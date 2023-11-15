39f1bae2edcdad815d0d111b3b025a2b52836c55
keniaag <124410749+keniaag@users.noreply.github.com>
2023-11-14 18:27:34 -0700

    Add files via upload

--- parcial3/GuardarMateria.php
+++ parcial3/GuardarMateria.php
@@ -1,18 +1,16 @@
-<?php 
-  include 'conexion.php';
+<?php
+    include 'conexion.php';
 
-  $nombre = $_POST["nombre"];
-  $semestre = $_POST["semestre"];
-  $especialidad = $_POST["especialidad"];
+    $nombre=$_POST["nombre"];
+    $semestre=$_POST["semestre"];
+    $especialidad=$_POST["especialidad"];
 
-  $sql = "INSERT INTO materia(nombre, semestre, especialidad) VALUES ('".$nombres."', ".$semestre.", '".$especialidad."')";
-
-  if(conexion->query($sql) === TRUE){
-    header("Location: ConsultarMaterias.php");
-    $conexion->close();
-    exit;
-  }else{
-    echo"<h2>Ocurrió un error</h2><p>Error: " .$sql . "<br>" .$conexion->error . "<p>";
-    echo"<h3><a href='ConsultarMaterias.php'>Regresar a materias</a></h3>";
-  }
-?>
+    $sql= "INSERT INTO materias(nombre, semestre, especialidad) VALUES ('".$nombre."',".$semestre.",'".$especialidad."')";
+    if($conexion->query($sql) === TRUE){
+        header("Location: ConsultarMaterias.php");
+        $conexion->close();
+        exit;
+    }else{
+        echo "<h2>Ocurrió un error</h2> <p>Error: ".$sql . "<br>" . $conexion->error . "</p>";
+        echo "<h3><a href='RegistrarMaterias.php'>Regresar a materias</a></h3>";
+    }
--- parcial3/RegistrarMaterias.php
+++ parcial3/RegistrarMaterias.php
@@ -13,7 +13,7 @@
     <div class="container">
         <div class="row">
             <div class="col-12 card m-4 p-4">
-            <form action="">
+            <form action="ConsultarMaterias.php" method="POST">
                     <div class="form-group">
                         <label for="">Nombre:</label>
                         <input name="nombre" type="text" class="form-control"  name= "nombre" placeholder="Teclea la materia" required>
@@ -22,7 +22,7 @@
                         <label for="">Semestre:</label>
                         <input name="nombre" type="text" class="form-control" name="semestre" placeholder="Teclea el semestre" required>
                     </div>
-                    <div class="form-group">
+                    <div class="form-group"><br>
                         <select name="ESPECIALIDAD" class="form-control">
                             <option value="">Seleccione una especialidad</option>
                             <option value="PROGRAMACION">PROGRAMACION</option>
@@ -32,11 +32,17 @@
                             <option value="CONSTRUCCION">CONSTRUCCION</option>
 
                         </select>
-</div>
-</form>
-</div>
-</div>
-</div>
+                    </div><br>
+                    <div>
+                        <input type="submit" class="btn btn-primary" value="Registrar">
+                        <a href="ConsultarMaterias.php" class="btn btn-danger">Cancelar</a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <script src="js/bootstrap.js"></script>
 </body>
 </html>
 
